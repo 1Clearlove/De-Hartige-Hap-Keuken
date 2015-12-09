67d3f8bbd32d360cff3f5ac7e92e9c36a262c63f
Tim Occhipinti <t.occhipinti@outlook.com>
2015-12-09 11:59:34 +0100

    Made changes to login panel to make it work again

--- src/main/java/dhh/main/Main.java
+++ src/main/java/dhh/main/Main.java
@@ -1,15 +1,11 @@
 package dhh.main;
 
 import dhh.presentation.LoginFrame;
-import dhh.presentation.ManageDishesFrame; //Enkel voor testgebruik
 
 public class Main {
 
     public static void main(String[] args) {
-        //LoginFrame frame = new LoginFrame();
-        //frame.createPanel();
-        
-        ManageDishesFrame frame = new ManageDishesFrame(); //Enkel voor testgebruik
-        frame.createPanel(); //Enkel voor testgebruik
+        LoginFrame frame = new LoginFrame();
+        frame.createPanel();
         }
 }
